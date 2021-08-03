90f76c3ebcb5c986eadf87959c1e178fee82133b
Rohit Jain <rohitjain19060@gmail.com>
2021-08-03 09:01:19 +0530

    Named Routes done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # meals_app
+ 
---
+++ lib/models/meal.dart
@@ -0,0 +1,16 @@
+enum Complexity {
+  Simple,
+  Challenging,
+  Hard,
+}
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imgageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final int completexity;
+}
