c37dfb768c750d615d20bc523f203a69df45be30
Rohit19060 <rohitjain19060@gmail.com>
2021-08-11 11:36:57 +0530

    Meal Models Done

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -13,14 +14,17 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] ?? "";
     final categoryId = routeArgs['id'];
+    final categroyMeals =
+        DUMMY_MEALS.where((e) => e.categories.contains(categoryId)).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Container();
+          return Text(categroyMeals[index].title);
         },
+        itemCount: categroyMeals.length,
       ),
     );
   }
--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -4,13 +4,40 @@ enum Complexity {
   Hard,
 }
 
+enum Affordability {
+  Affordable,
+  Pricey,
+  Luxurious,
+}
+
 class Meal {
   final String id;
   final List<String> categories;
   final String title;
-  final String imgageUrl;
+  final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
   final int duration;
-  final int completexity;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Meal({
+    required this.categories,
+    required this.title,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.steps,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+    required this.isGlutenFree,
+    required this.isVegan,
+    required this.isVegetarian,
+    required this.isLactoseFree,
+    required this.id,
+  });
 }
